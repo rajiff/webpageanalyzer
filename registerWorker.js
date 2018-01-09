117cfa8f02106c39e39cec196925669fc130c9f1
Basavaraj K N <Basavaraj.K@Stackroute.in>
2018-01-09 19:42:13 +0530

    Initial commit

---
+++ registerWorker.js
@@ -0,0 +1,9 @@
+const async = require('async');
+
+const registerWorker = function(eventName, worker) {
+	// Connect to message broker
+	// Listen for the specific event
+	// When message is received, invoke the worker
+}
+
+module.exports = registerWorker;
