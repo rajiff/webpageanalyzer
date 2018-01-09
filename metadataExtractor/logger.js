d9787d5367994d4708468662bf7ee1b6be40f3cd
Basavaraj K N <Basavaraj.K@Stackroute.in>
2018-01-09 20:50:27 +0530

    logging set right

--- metadataExtractor/logger.js
+++ metadataExtractor/logger.js
@@ -1,8 +1,8 @@
 const log4js = require('log4js');
 
 log4js.configure({
-  appenders: { default: { type: 'file', filename: 'application.log' }, console: { type: 'console' } },
-  categories: { default: { appenders: ['default', 'console'], level: 'error' } }
+  appenders: { logfile: { type: 'file', filename: 'application.log' }, console: { type: 'console' } },
+  categories: { default: { appenders: ['logfile', 'console'], level: 'debug' } }
 });
 
 const logger = log4js.getLogger('default');
